6540cc3d6ea9aea54d36f8885d49d3804e1b6507
tn00gm <tn00gm@gmail.com>
2014-04-13 17:47:33 +0900

    fix Resource#equals
    for faster execution

--- rdf-store/src/main/java/core/Resource.java
+++ rdf-store/src/main/java/core/Resource.java
@@ -36,24 +36,7 @@ public class Resource implements Token {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Resource other = (Resource) obj;
-		if (name == null) {
-			if (other.name != null) {
-				return false;
-			}
-		} else if (!name.equals(other.name)) {
-			return false;
-		}
-		return true;
+		return this == obj;
 	}
 
 	@Override
