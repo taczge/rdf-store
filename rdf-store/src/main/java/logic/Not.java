385fc73ec2c086adc77396ebbe75ef0a0ad32c9a
tn00gm <tn00gm@gmail.com>
2014-04-16 14:42:13 +0900

    implement PLParser

---
+++ rdf-store/src/main/java/logic/Not.java
@@ -0,0 +1,47 @@
+package logic;
+
+public class Not implements Proposition {
+
+	private final Proposition p;
+
+	public Not(Proposition p) {
+		super();
+		this.p = p;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((p == null) ? 0 : p.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Not other = (Not) obj;
+		if (p == null) {
+			if (other.p != null) {
+				return false;
+			}
+		} else if (!p.equals(other.p)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return String.format( "(not %s)", p );
+	}
+
+}
